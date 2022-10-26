64e70a16c8eae5ce236b04ab997691df5c486566
EdrickDefeo <86871115+spier003@users.noreply.github.com>
2022-10-26 16:11:50 +0800

    food
    food

--- PracticalExam-CC/src/app/food-planner/food-planner.page.ts
+++ PracticalExam-CC/src/app/food-planner/food-planner.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-food-planner',
   templateUrl: './food-planner.page.html',
   styleUrls: ['./food-planner.page.scss'],
 })
-export class FoodPlannerPage implements OnInit {
+// export class FoodPlannerPage implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
+//   ngOnInit() {
+//   }
+
+// }
+export class FoodPlannerPage {
+  @ViewChild(IonModal) modal: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
 
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 }
