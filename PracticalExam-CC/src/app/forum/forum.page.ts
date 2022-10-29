bd535b76edce43219e1308e077f10e849c7c18f2
EdrickDefeo <86871115+spier003@users.noreply.github.com>
2022-10-29 08:21:51 +0800

    forum added - paconnect lods yung forum button sa forum html pag-alam mo
    forum added

--- PracticalExam-CC/src/app/forum/forum.page.ts
+++ PracticalExam-CC/src/app/forum/forum.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forum.page.scss'],
 })
 export class ForumPage implements OnInit {
-
+  segment = 'second';
   constructor() { }
 
   ngOnInit() {
